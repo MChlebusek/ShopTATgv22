866104ed52298f162625bb5b7136733e18bef754
Opilane TTHK <118188541+MChlebusek@users.noreply.github.com>
2023-11-07 12:07:12 +0200

    Weather
    Weather

---
+++ Shop/Shop.ApplicationServices/Services/WeatherForecastServices.cs
@@ -0,0 +1,32 @@
+﻿using Nancy.Json;
+using Shop.Core.Dto.OpenWeatherDtos;
+using System.Net;
+
+namespace Shop.ApplicationServices.Services
+{
+    public class WeatherForecastServices
+    {
+
+        public async Task<OpenWeatherResultDto> OpenWeatherResul(OpenWeatherResultDto dto)
+        {
+            string idOpenWeather = "cba31897ad31194044f974d2b13d65c6";
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={dto.City}&units=metric&appid={idOpenWeather}";
+
+            using (WebClient client = new WebClient())
+            {
+                string json = client.DownloadString(url);
+                OpenWeatherResponseRootDto weatherResult = new JavaScriptSerializer().Deserialize<OpenWeatherResponseRootDto>(json);
+
+                dto.City = weatherResult.Name;
+                dto.Temp = weatherResult.Main.Temp;
+                dto.FeelsLike = weatherResult.Main.Feels_like;
+                dto.Humidity = weatherResult.Main.Humidity;
+                dto.Pressure = weatherResult.Main.Pressure;
+                dto.WindSpeed = weatherResult.Wind.Speed;
+                dto.Description = weatherResult.Weather[0].Description;
+            }
+
+            return dto;
+        }
+    }
+}
