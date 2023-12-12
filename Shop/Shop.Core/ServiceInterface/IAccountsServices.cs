2a437143e2b0cec6a3a7e61cbb2b02b298bddc98
Opilane TTHK <118188541+MChlebusek@users.noreply.github.com>
2023-12-12 12:37:17 +0200

    Login/Register
    Login/Register

---
+++ Shop/Shop.Core/ServiceInterface/IAccountsServices.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shop.Core.Domain;
+using Shop.Core.Dto.AccountsDtos;
+
+namespace Shop.Core.ServiceInterface
+{
+    public interface IAccountsServices
+    {
+        Task<ApplicationUser> Register(ApplicationUserDto dto);
+    }
+}
---
+++ Shop/Shop/Models/Accounts/AddPasswordViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Models.Accounts
+{
+    public class AddPasswordViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "New Password")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "New Password")]
+        [Compare("NewPassword", ErrorMessage = "Password mismatch.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
